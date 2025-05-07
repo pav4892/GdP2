e75b5379fc39a2fc5e6dee49964e3d3dc7a7cf90
Some Student. Put your name here <your.email@your.provider>
2025-05-07 15:01:36 +0200

    getting there

--- pa3/route.cpp
+++ pa3/route.cpp
@@ -1,26 +1,28 @@
-#include <functional>
 #include "route.h"
+#include "vertical.h"
+#include <algorithm>
+#include <limits>
 
 using namespace std;
 
 Route::Route(const float pHeight, const function<float(float, float, float, float, float)> pDist) {
-  setHeight(pHeight);
-  setDist(pDist);
+  height = pHeight;
+  dist = pDist;
 };
 
-Route::Route(Route& route) {
-  setHeight(route.height);
-  setDist(route.dist);
+Route::Route(const Route& route) {
+  destinations = route.destinations;
+  height = route.height;
+  dist = route.dist;
 };
 
-Route::~Route(){};
+Route::~Route() {};
 
 void Route::add(const float destX, const float destY) {
-  const vector<pair<float, float>> elem; // Here we cannot just use <float, float> but need <pair<float,float>> becauuuuuse
-  destinations.push_back(elem); // Add element to end of destinations vector
+  destinations.emplace_back(destX, destY);
 };
 
-const vector<pair<float, float>>& Route::getDestinations() {
+const vector<pair<float, float>>& Route::getDestinations() const {
   return destinations;
 };
 
@@ -32,58 +34,69 @@ void Route::setHeight(const float pHeight) {
   height = pHeight;
 };
 
-function<float(float, float, float, float, float)> Route::getDist() const {
-  return dist;
-};
-
-void Route::setDist(function<float(float, float, float, float, float)> pDist) {
+void Route::setDist(const function<float(float, float, float, float, float)> pDist) {
   dist = pDist;
 };
 
-const float Route::distance() const { // not same as in vertical... check ich nicht, fix later. This is supposed to return the total distance that it will take to sequentially go through the destinations vector not just from point 
-                               // A to B like distance() of vertical.cpp but the total route from start to finish that is saved in the destinations vector
-  float distanceTotal = 0;
 
-  int destinationsArrLength = sizeof(destinations)/sizeof(destinations[0]);
+const float Route::distance() const {
+    float distanceTotal = 0.0;
+    int distanceArrLength = destinations.size(); // Get arrayLength for iteratior later...
 
-  if(destinationsArrLength > 0) {
-    distanceTotal = Vertical::distance(0.0, 0.0, destinations[0][0], destinations[0][1], getHeight()); //calc startflight
+    if(distanceArrLength > 0) {
 
-    if(destinationsArrLength > 1) {
-      for(int i = 0; i <= destinationsArrLength - 1; i++) {
-        distanceTotal += Vertical::distance(destinations[i][0], destinations[i][1], destinations[i+1][0], destinations[i+1][1], getHeight()); 
-      }
+        // Start flight from origin to first
+        distanceTotal += dist(0.0, 0.0, destinations[0].first, destinations[0].second, height);
+
+        if(distanceArrLength > 1) {
+            // Intermediate flights
+            for (int i = 0; i < distanceArrLength - 1; ++i) {
+                distanceTotal += dist(destinations[i].first, destinations[i].second, destinations[i + 1].first, destinations[i + 1].second, height);
+            }
+        }
+
+        // Return flight
+        distanceTotal += dist(destinations.back().first, destinations.back().second, 0.0, 0.0, height);
+    } else {
+        distanceTotal = 0.0;
     }
 
-    //calc backflight
-    distanceTotal += Vertical::distance(destinations.back()[0], destinations.back()[1], 0.0, 0.0, getHeight()); 
-  } else {
-    distanceTotal = 0.0;
-  }
+    return distanceTotal;
+}
 
-  return distanceTotal;
-};
+const Route Route::shortestRoute() const {
+    if (destinations.empty()) return *this;
 
-const Route Route::shortestRoute() {
-  //Reorder destinations vector to optimize for shortest flight time. Do this via bruteforce-approach
-  
-  float lowestDistance = 10000000000000;
+    vector<pair<float, float>> perm = destinations;
+    vector<pair<float, float>> bestPerm;
+    float bestDist = numeric_limits<float>::max();
 
-  int destinationsArrLength = sizeof(destinations)/sizeof(destinations[0]);
+    sort(perm.begin(), perm.end());
 
-  sort(destinations->begin(), destinations->end());
+    do {
+        float tempDist = 0.0f;
 
-  while(std::next_permutation(destinations->begin(), destinations->end())) {
-    if(distance() < lowestDistance) {
-      lowestDistance = distance();
+        tempDist += dist(0.0f, 0.0f, perm[0].first, perm[0].second, height);
+        for (size_t i = 0; i < perm.size() - 1; ++i) {
+            tempDist += dist(
+                perm[i].first, perm[i].second,
+                perm[i + 1].first, perm[i + 1].second,
+                height
+            );
+        }
+        tempDist += dist(perm.back().first, perm.back().second, 0.0f, 0.0f, height);
+
+        if (tempDist < bestDist) {
+            bestDist = tempDist;
+            bestPerm = perm;
+        }
+    } while (next_permutation(perm.begin(), perm.end()));
+
+    Route best(height, dist);
+    for (const auto& p : bestPerm) {
+        best.add(p.first, p.second);
     }
-  }
 
-  Route idealRoute = new Route(getHeight(), dist);
+    return best;
+}
 
-  for(int i = 0; i <= destinationsArrLength - 1; i++) {
-    idealRoute.add(idealDestinationsArray[i][0], idealDestinationsArray[i][1]);
-  }
-
-  return idealRoute; //Kopie von Route object mit destinations object in idealer Ordnung
-};
--- pa3/route.h
+++ pa3/route.h
@@ -1,24 +1,28 @@
+#pragma once
+
 #include <functional>
-#include "vertical.h"
+#include <vector>
+#include <utility>
 
 using namespace std;
 
 class Route {
-
-  private:
-    const vector<pair<float, float>>* destinations;   // the floats, pairs etc in here cannot be const, just like any vector content-type-definition
+private:
+    vector<pair<float, float>> destinations;
     float height;
-    const function<float(float, float, float, float, float)> dist; //function template for any method/function that takes in 5 floats and returns 1 float
-  public:
-    Route(const float pHeight, const function<float(float, float, float, float, float)> pDist);  // Constructor 1 
-    Route(Route& route);                               // Constructor 2 - Use more than 1 constructor via constructor overloading, which constructor is chosen depends on the Uebergabe-parameter given to it. Here the second
-                                                       // Constructor will only be called if a copy of the instance of this class is created, e.g. via Route route2 = route1; or something. This is called copy-constructor
+    function<float(float, float, float, float, float)> dist;
+
+public:
+    Route(float pHeight, function<float(float, float, float, float, float)> pDist); // Constructor 1 
+    Route(const Route& route); // Copy constructor
     ~Route();
+
     void add(const float destX, const float destY);
-    const vector<pair<float, float>>& getDestinations();
+    const vector<pair<float, float>>& getDestinations() const;
     const float getHeight() const;
     void setHeight(const float pHeight);
     void setDist(function<float(float, float, float, float, float)> pDist);
     const float distance() const;
-    const Route shortestRoute();
+    const Route shortestRoute() const;
 };
+
