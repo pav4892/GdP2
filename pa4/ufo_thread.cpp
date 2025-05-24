9320f8157c4a1ed41251a16892ae14886f24ec31
Some Student. Put your name here <your.email@your.provider>
2025-05-24 13:55:17 +0200

    pa4 fertig - fix - 2

--- pa4/ufo_thread.cpp
+++ pa4/ufo_thread.cpp
@@ -12,29 +12,22 @@ UfoThread::UfoThread(Ufo * pUfo) {
 UfoThread::~UfoThread(){
   if(flyThread != nullptr) {
     (*flyThread).join();
+    delete flyThread;
+    flyThread = nullptr;
   };
-  delete flyThread;
-  flyThread = nullptr;
+  
   isFlying = false;
 };
 
 void UfoThread::runner(const float x, const float y, const float height, const int speed) {
   ufo->flyToDest(x, y, height, speed);
   isFlying = false;
-
-  if(flyThread != nullptr) {
-    (*flyThread).join();    
-    delete flyThread;
-    flyThread = nullptr;
-  };
 };
 
 void UfoThread::startUfo(const float x, const float y, const float height, const int speed) {
 
-  // We do the condition twice here because the instruction says it neeeds to be joined after it ends obvi BUT also in case another thread still exists.... somehow, not sure if I need the second check here or in destructor
-  
-  if(flyThread != nullptr) {
-    (*flyThread).join();    
+  if(flyThread != nullptr) { // In case another thread still exists and we call startufoo twice and the other one hasnt been killeld 5yet because the destructor wasn't called
+    (*flyThread).join();
     delete flyThread;
     flyThread = nullptr;
   };
@@ -44,6 +37,6 @@ void UfoThread::startUfo(const float x, const float y, const float height, const
   
 };
 
-const bool UfoThread::getIsFlying() const {
+bool UfoThread::getIsFlying() const {
   return isFlying;
 };
