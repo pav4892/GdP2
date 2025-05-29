0fd5944dbb53ebd93fd9f192460aa91e03356641
Some Student. Put your name here <your.email@your.provider>
2025-05-29 19:49:17 +0200

    pa5 gui with functionality til d)  kinda e) f) done

--- pa5/ui_widget.h
+++ pa5/ui_widget.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QtWidgets>
+#include <time.h>
+#include <unistd.h>
 #include "ufosim.h"
 #include "vertical.h"
 #include "ufo_thread.h"
@@ -11,119 +13,149 @@ class MainWidget : public QWidget
 {
     Q_OBJECT
 
-public:
-    MainWidget(QMainWindow *parent = nullptr): QWidget(parent)
-    {
+    public:
+        MainWidget(QMainWindow *parent = nullptr): QWidget(parent)
+        {
 
-        ufo = new Vertical("r2d2");
-        uthread = new UfoThread(ufo);
+            ufo = new Vertical("r2d2");
+            uthread = new UfoThread(ufo);
 
-        label1 = new QLabel("x: ");
-        label2 = new QLabel("y: ");
-        label3 = new QLabel("height: ");
-        label4 = new QLabel("speed: ");
+            label1 = new QLabel("x: ");
+            label2 = new QLabel("y: ");
+            label3 = new QLabel("height: ");
+            label4 = new QLabel("speed: ");
 
-        inputX = new QLineEdit();
-        inputY = new QLineEdit();
-        inputHeight = new QLineEdit();
-        inputSpeed = new QLineEdit();
+            inputX = new QLineEdit();
+            inputY = new QLineEdit();
+            inputHeight = new QLineEdit();
+            inputSpeed = new QLineEdit();
 
-        inputX->setFixedSize(150, 30);
-        inputY->setFixedSize(150, 30);
-        inputHeight->setFixedSize(150, 30);
-        inputSpeed->setFixedSize(150, 30);
+            inputX->setFixedSize(150, 30);
+            inputY->setFixedSize(150, 30);
+            inputHeight->setFixedSize(150, 30);
+            inputSpeed->setFixedSize(150, 30);
 
-        startButton = new QPushButton("Start");
-        startButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-        startButton->setFixedHeight(40);
-        
-        connect(startButton, SIGNAL(clicked()), this, SLOT(startUfo()));
+            startButton = new QPushButton("Start");
+            startButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+            startButton->setFixedHeight(40);
+            
+            connect(startButton, SIGNAL(clicked()), this, SLOT(startUfo()));
 
-        infoLabel = new QLabel("\nStarted at\nPosition:\n0.00 | 0.00 | 0.00 meter\n");
-        infoLabel->setWordWrap(true);
-        infoLabel->setAlignment(Qt::AlignCenter);
-        infoLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+            infoLabel = new QLabel("\nStarted at\nPosition:\n0.00 | 0.00 | 0.00 meter\n");
+            infoLabel->setWordWrap(true);
+            infoLabel->setAlignment(Qt::AlignCenter);
+            infoLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 
-        // Do the frame around it (is from moodle)
-        infoLabel->setFrameShape(QFrame::Box);
-        infoLabel->setFrameShadow(QFrame::Plain);
+            // Do the frame around it (is from moodle)
+            infoLabel->setFrameShape(QFrame::Box);
+            infoLabel->setFrameShadow(QFrame::Plain);
 
-        QGridLayout *formLayout = new QGridLayout();
-        formLayout->addWidget(label1, 0, 0);
-        formLayout->addWidget(inputX, 0, 1);
-        formLayout->addWidget(label2, 1, 0);
-        formLayout->addWidget(inputY, 1, 1);
-        formLayout->addWidget(label3, 2, 0);
-        formLayout->addWidget(inputHeight, 2, 1);
-        formLayout->addWidget(label4, 3, 0);
-        formLayout->addWidget(inputSpeed, 3, 1);
-        formLayout->setSpacing(10);
+            QGridLayout *formLayout = new QGridLayout();
+            formLayout->addWidget(label1, 0, 0);
+            formLayout->addWidget(inputX, 0, 1);
+            formLayout->addWidget(label2, 1, 0);
+            formLayout->addWidget(inputY, 1, 1);
+            formLayout->addWidget(label3, 2, 0);
+            formLayout->addWidget(inputHeight, 2, 1);
+            formLayout->addWidget(label4, 3, 0);
+            formLayout->addWidget(inputSpeed, 3, 1);
+            formLayout->setSpacing(10);
 
-        QVBoxLayout *mainLayout = new QVBoxLayout();
-        mainLayout->addLayout(formLayout);
-        mainLayout->addStretch();
-        mainLayout->addWidget(startButton);
-        mainLayout->addWidget(infoLabel);
+            QVBoxLayout *mainLayout = new QVBoxLayout();
+            mainLayout->addLayout(formLayout);
+            mainLayout->addStretch();
+            mainLayout->addWidget(startButton);
+            mainLayout->addWidget(infoLabel);
 
-        setLayout(mainLayout);
-    }
+            setLayout(mainLayout);
+        }
 
-    ~MainWidget()
-    {
-        delete label1;
-        delete label2;
-        delete label3;
-        delete label4;
+        ~MainWidget()
+        {
 
-        delete inputX;
-        delete inputY;
-        delete inputHeight;
-        delete inputSpeed;
+            //TODO Clean everything up here
 
-        delete startButton;
-        delete infoLabel;
-    }
+            delete label1;
+            delete label2;
+            delete label3;
+            delete label4;
 
-private slots:
-    void startUfo() {
-        QString xStr = inputX->text();
-        QString yStr = inputY->text();
-        QString heightStr = inputHeight->text();
-        QString speedStr = inputSpeed->text();
+            delete inputX;
+            delete inputY;
+            delete inputHeight;
+            delete inputSpeed;
 
-        double xDouble = xStr.toDouble();
-        double yDouble = yStr.toDouble();
-        double heightDouble = heightStr.toDouble();
-        double speedDouble = speedStr.toDouble();
+            delete startButton;
+            delete infoLabel;
+        }
 
-        float x = static_cast<float>(xDouble);
-        float y = static_cast<float>(yDouble);
-        float height = static_cast<float>(heightDouble);
-        int speed = static_cast<int>(speedDouble);
+    private slots:
+        void startUfo() {
+            // Get and Cast the QStrings to the appropriate datatypes for use in uthread's startUfo() function later
+            
+            // We pass a declared but uninitialised boolean to the toFloat function as error detection because it will set it to false if there is a problem with the conversion and dont have to deal with try catch. Source: google
 
-        startButton->setText("Flying");
-        startButton->setEnabled(false);
+            bool okX;
+            QString xStr = inputX->text();
+            float x = xStr.toFloat(&okX);
+            if(!okX) {
+                inputX->setText("error: must be float");
+            };
 
-        uthread->startUfo(x, y, height, speed);
-    };
+            bool okY;
+            QString yStr = inputY->text();
+            float y = yStr.toFloat(&okY);
+            if(!okY) {
+                inputY->setText("error: must be float");
+            };
 
-private:
+            bool okHeight;
+            QString heightStr = inputHeight->text();
+            float height = heightStr.toFloat(&okHeight);
+            if(!okHeight) {
+                inputHeight->setText("error: must be float");
+            };
 
-    Ufo * ufo;
-    UfoThread * uthread;
+            bool okSpeed;
+            QString speedStr = inputSpeed->text();
+            int speed = speedStr.toInt(&okSpeed);
+            if(!okSpeed) {
+                inputSpeed->setText("error: must be int");
+            };
 
-    QLabel *label1;
-    QLabel *label2;
-    QLabel *label3;
-    QLabel *label4;
+            if(okX && okY && okHeight && okSpeed) {
 
-    QLineEdit *inputX;
-    QLineEdit *inputY;
-    QLineEdit *inputHeight;
-    QLineEdit *inputSpeed;
+                startButton->setText("Flying");
+                startButton->setEnabled(false);
 
-    QPushButton *startButton;
-    QLabel *infoLabel;
+                uthread->startUfo(x, y, height, speed);
+
+                while(1) {
+                    if(uthread->getIsFlying() == false) {
+                        printf("hey\n");
+                        break;
+                    }
+                }
+            };
+        };
+
+    private:
+
+        Ufo * ufo;
+        UfoThread * uthread;
+
+        QLabel *label1;
+        QLabel *label2;
+        QLabel *label3;
+        QLabel *label4;
+
+        QLineEdit *inputX;
+        QLineEdit *inputY;
+        QLineEdit *inputHeight;
+        QLineEdit *inputSpeed;
+
+        QPushButton *startButton;
+        QLabel *infoLabel;
 };
 
 #endif // MAINWIDGET_H
